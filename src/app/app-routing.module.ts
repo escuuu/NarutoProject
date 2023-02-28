278472a2525be7474a3b8d921d5184e391c6cd66
Alejandro Escuredo Fernandez <alejandro.escudero@mulesol.es>
2023-02-28 20:43:27 +0100

    Creacion de proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PortadaComponent } from './components/portada/portada.component';
+import { VillasComponent } from './components/villas/villas.component';
+import { WikiComponent } from './components/wiki/wiki.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: PortadaComponent },
+  { path: 'wiki', component: WikiComponent },
+  { path: 'villas', component: VillasComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PortadaComponent } from './components/portada/portada.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { WikiComponent } from './components/wiki/wiki.component';
+import { VillasComponent } from './components/villas/villas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortadaComponent,
+    NavbarComponent,
+    WikiComponent,
+    VillasComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
